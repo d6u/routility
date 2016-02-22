83aebb3c76687fad2f9106a42a8753cf7ebd74dc
Daiwei <daiweilu123@gmail.com>
2016-02-21 21:25:34 -0500

    Add basic test for start function

--- test/src/index.spec.js
+++ test/src/index.spec.js
@@ -118,3 +118,152 @@ describe('parse', function () {
   });
 
 });
+
+describe('start', function () {
+
+  const routes = (
+    r('/', 'root', [
+      redirect('/', '/sign-up'),
+      redirect('/sign-up', '/login'),
+      r('/login', 'login'),
+      r('/user/:id', 'user', [
+        r('/', 'index'),
+        r('/profile', 'profile')
+      ]),
+      r('/about/*path', 'about', [
+        r('/', 'index'),
+        r('/detail', 'detail')
+      ]),
+    ])
+  );
+
+  it('invokes handler with initial state', function (done) {
+    start(r('/', 'root'), function (state) {
+      try {
+        expect(state).toEqual({
+          root: {},
+          queryParams: {}
+        });
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+  });
+
+  it('invokes handler with initial redirect state', function (done) {
+    start(routes, function (state) {
+      try {
+        expect(state).toEqual({
+          root: {
+            login: {}
+          },
+          queryParams: {},
+          redirectTo: '/login',
+          redirectFrom: '/',
+        });
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+  });
+
+  it('invokes handler with new state when use navTo helper', function (done) {
+    let skippedInitial = false;
+
+    const navTo = start(routes, function (state) {
+      if (!skippedInitial) {
+        skippedInitial = true;
+        return;
+      }
+
+      try {
+        expect(state).toEqual({
+          root: {
+            user: {
+              id: '123',
+              index: {},
+            }
+          },
+          queryParams: {},
+        });
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+
+    navTo('/user/123');
+  });
+
+  it('invokes handler with new state when navTo hit redirect route', function (done) {
+    let skippedInitial = false;
+
+    const navTo = start(routes, function (state) {
+      if (!skippedInitial) {
+        skippedInitial = true;
+        return;
+      }
+
+      try {
+        expect(state).toEqual({
+          root: {
+            login: {}
+          },
+          queryParams: {},
+          redirectTo: '/login',
+          redirectFrom: '/sign-up',
+        });
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+
+    navTo('/sign-up');
+  });
+
+  it('navTo returns new state when called with new path', function () {
+    const navTo = start(routes, function (state) {});
+    const result = navTo('/user/123');
+
+    expect(result).toEqual({
+      root: {
+        user: {
+          id: '123',
+          index: {}
+        }
+      },
+      queryParams: {},
+    });
+  });
+
+  it('navTo returns new state when hit redirect route', function () {
+    const navTo = start(routes, function (state) {});
+    const result = navTo('/sign-up');
+
+    expect(result).toEqual({
+      root: {
+        login: {}
+      },
+      queryParams: {},
+      redirectTo: '/login',
+      redirectFrom: '/sign-up',
+    });
+  });
+
+  it('navTo to same path should not ', function () {
+    let skippedInitial = false;
+    const navTo = start(routes, function (state) {
+      if (!skippedInitial) {
+        skippedInitial = true;
+        return;
+      }
+      throw new Error('should not invoke');
+    });
+    navTo('/login');
+    navTo('/login');
+  });
+
+});
